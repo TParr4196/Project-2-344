c69bcbb4fa4ae276cbd70dae23f11d4d401548c9
Tyler Parr <parrri@oregonstate.edu>
2022-01-16 13:13:08 -0800

    shake the rust off

--- hello.c
+++ hello.c
@@ -4,5 +4,11 @@ int main(int argc, const char *argv[]) {
   int x = 12;
   float y = 3.14;
   char* s = "Hello, world!";
-  printf("x is %d, y is %f", x, y);
+  printf("x is %d, y is %f\n", x, y);
+  printf("%s\n", s);
+  int i = 0;
+  while(i<=4){
+    printf("%d x 5 = %d\n", i, i*5);
+    i++;
+  }
 }
